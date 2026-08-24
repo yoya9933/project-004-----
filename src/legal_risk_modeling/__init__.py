501c32f9953a6f3a6d24ee8d2b2985d726e406fe
Yu <124881761+yoya9933@users.noreply.github.com>
2026-08-24 17:11:17 +0800

    Fix package promotion exports

--- src/legal_risk_modeling/__init__.py
+++ src/legal_risk_modeling/__init__.py
@@ -1,7 +1,7 @@
 """Shared source-of-truth modules for legal penalty risk modeling."""
 
-from .features import FEATURE_NAMES, ISSUE_FIELDS, case_splits, build_feature_frame
-from .promotion import build_promotion_report, load_metrics_csv
+from .features import FEATURE_NAMES, ISSUE_FIELDS, build_feature_frame, case_splits
+from .promotion import build_promotion_report
 
 __all__ = [
     "FEATURE_NAMES",
@@ -9,5 +9,4 @@ __all__ = [
     "case_splits",
     "build_feature_frame",
     "build_promotion_report",
-    "load_metrics_csv",
 ]
